ac6ea784863348aa87a708250ab538ec084916c5
lunevaes <luneveks@gmail.com>
2020-02-16 18:40:58 +0300

    Изменяет верстку согласно БЭМ

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-var link = document.querySelector(".contacts-data .button");
-var popup = document.querySelector(".modal-feedback");
-var close = popup.querySelector(".modal-close");
+var link = document.querySelector(".contacts__data .button");
+var popup = document.querySelector(".modal--feedback");
+var close = popup.querySelector(".modal__close");
 var form = popup.querySelector("form");
 var login = popup.querySelector("[name=login]");
 var email = popup.querySelector("[name=email]");
 var storage = localStorage.getItem("login");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
-  popup.classList.remove("modal-error");
+  popup.classList.add("modal--show");
+  popup.classList.remove("modal__error");
   if (storage) {
     login.value = storage;
     password.focus();
@@ -18,41 +18,41 @@ link.addEventListener("click", function (evt) {
 });
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  popup.classList.remove("modal--show");
 });
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value) {
   evt.preventDefault();
-  popup.classList.remove("modal-error");
+  popup.classList.remove("modal__error");
   popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
+  popup.classList.add("modal__error");
   } else {
     localStorage.setItem("login", login.value);
   }
 });
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
+    if (popup.classList.contains("modal--show")) {
+    popup.classList.remove("modal--show");
+    popup.classList.remove("modal__error");
     }
   }
 });
  var mapLink = document.querySelector(".contacts-button-map");
- var mapPopup = document.querySelector(".modal-map");
- var mapClose = mapPopup.querySelector(".modal-close");
+ var mapPopup = document.querySelector(".map");
+ var mapClose = mapPopup.querySelector(".modal__close");
  mapLink.addEventListener("click", function (evt) {
    evt.preventDefault();
-   mapPopup.classList.add("modal-show");
+   mapPopup.classList.add("modal--show");
  });
  mapClose.addEventListener("click", function (evt) {
    evt.preventDefault();
-   mapPopup.classList.remove("modal-show");
+   mapPopup.classList.remove("modal--show");
  });
  window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
+    if (mapPopup.classList.contains("modal--show")) {
+      mapPopup.classList.remove("modal--show");
     }
   }
  });
